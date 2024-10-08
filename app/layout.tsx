066d4a270d9674ad19d0acabf5c108d04794681b
Jayasri9786 <pjayasri815@gmail.com>
2024-10-08 11:04:04 +0530

    update page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"], 
@@ -22,7 +23,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable )}>{children} </body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable )}>{children} 
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+          >
+          {children}
+      </ThemeProvider>
+      </body>
+
     </html>
   );
 }
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+module. exports = {
+  plugins: ["tailwindcss", "postcss-nesting", "autoprefixer"],
 };
-
-export default config;
